f515b19a241e3efb8e24cc271bd503ec1d463df3
ovVrLFg8ks <122876910+ovVrLFg8ks@users.noreply.github.com>
2023-11-10 11:06:36 +0300

    Update Doubly Linked List STATIC.cxx

--- Doubly Linked List STATIC.cxx
+++ Doubly Linked List STATIC.cxx
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
-class LinkedList{
+class LinkedList {
     int element;
-    LinkedList * next = NULL;
-    LinkedList * prev = NULL;
-    static LinkedList * head;
-    static LinkedList * tail;
+    LinkedList* next = NULL;
+    LinkedList* prev = NULL;
+    static LinkedList* head;
+    static LinkedList* tail;
     static int counter;
-    
+
     LinkedList(int n) : element(n) {
         counter++;
     }
     ~LinkedList() {
         counter--;
     }
-    
+
     static LinkedList* searchIndx(int& i) {
         if (counter / 2 >= i) {
             LinkedList* t = head;
@@ -31,7 +31,7 @@ class LinkedList{
             return t;
         }
     }
-    
+
 public:
     static bool IsEmpty() {
         return counter == 0;
@@ -40,49 +40,52 @@ public:
         return counter;
     }
     static void print() {
-        if (!counter) return ;
-        LinkedList * p = head;
-        cout<<"head-> ";
+        if (!counter) { 
+            cout << "empty!" << endl;
+            return;
+        }
+        LinkedList* p = head;
+        cout << "head-> ";
         while (p) {
-            cout<<p->element<<" ";
+            cout << p->element << " ";
             p = p->next;
         }
-        cout<<"<-tail"<<endl;
+        cout << "<-tail" << endl;
     }
-    
+
     static bool push_front(int n) {
-       LinkedList * p = new LinkedList(n);
-       if (!p) return false;
-       if (counter == 1) { // there was a bug
-           head = p;
-           tail = p;
-       }
-       else {
-          head->prev = p;
-          p->next = head;
-          head = p;
-       }
-       return true;
-    }
-    //static bool push_back();
-    static bool push_back(int n) {
-       LinkedList * p = new LinkedList(n);
-       if (!p) return false;
-       if (counter == 1) {
-           head = p;
-           tail = p;
-       }
-       else {
-          tail->next = p;
-          p->prev = tail;
-          tail = p;
-       }
-       return true;
+        LinkedList* p = new LinkedList(n);
+        if (!p) return false;
+        if (counter == 1) { // there was a bug
+            head = p;
+            tail = p;
+        }
+        else {
+            head->prev = p;
+            p->next = head;
+            head = p;
+        }
+        return true;
     }
     
-    static bool pop_front(){
+    static bool push_back(int n) {
+        LinkedList* p = new LinkedList(n);
+        if (!p) return false;
+        if (counter == 1) {
+            head = p;
+            tail = p;
+        }
+        else {
+            tail->next = p;
+            p->prev = tail;
+            tail = p;
+        }
+        return true;
+    }
+
+    static bool pop_front() {
         if (IsEmpty()) return false;
-        LinkedList * p = head;
+        LinkedList* p = head;
         if (head == tail) head = tail = NULL;
         else {
             head = head->next;
@@ -91,10 +94,10 @@ public:
         delete p;
         return true;
     }
-    
+
     static bool pop_back() {
         if (IsEmpty()) return false;
-        LinkedList * p = head;
+        LinkedList* p = tail;
         if (head == tail) head = tail = NULL;
         else {
             tail = tail->prev;
@@ -103,22 +106,22 @@ public:
         delete p;
         return true;
     }
-    
+
     static bool delete_list() {
-    	if (IsEmpty()) return false;
-    	
-    	LinkedList * p1 = head;
-    	LinkedList * p2;
+        if (IsEmpty()) return false;
+
+        LinkedList* p1 = head;
+        LinkedList* p2;
         while (p1) {
             p2 = p1->next;
             delete p1;
             p1 = p2;
         }
         head = tail = NULL;
-    	
-    	return true;
+
+        return true;
     }
-    
+
     static void erase_element(int i) {
         if (i < 0 || i >= counter) {
             throw std::out_of_range("");
@@ -141,12 +144,16 @@ int main()
     LinkedList::push_front(10);
     LinkedList::push_front(20);
     LinkedList::push_back(30);
-    
     LinkedList::print();
-    
+
     LinkedList::erase_element(1);
-    
     LinkedList::print();
-    
-    return 0; 
+
+    LinkedList::pop_back();
+    LinkedList::print();
+
+    LinkedList::pop_front();
+    LinkedList::print();
+
+    return 0;
 }
