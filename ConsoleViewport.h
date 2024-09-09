a92a1969635fe49cef4295a7af70438e6c01d1a5
ovVrLFg8ks <122876910+ovVrLFg8ks@users.noreply.github.com>
2024-09-10 00:03:45 +0300

    ConsoleViewport linux support

--- ConsoleViewport.h
+++ ConsoleViewport.h
@@ -1,29 +1,47 @@
 #pragma once
 
-#include <Windows.h>
 #include <stdexcept>
 
+#ifdef _WIN32
+	#include <Windows.h>
+#elif __linux__
+	#include <sys/ioctl.h>
+#endif
+
+
 class ConsoleViewport {
+
 private:
+#ifdef _WIN32
     HANDLE hStdOut;
     CONSOLE_SCREEN_BUFFER_INFO csbi;
+#endif
     int width, height, widthP1, screenSize;
 
+
 #define WIDECHAR true
+
 #if WIDECHAR
 #define CHARTYPE wchar_t
-    CHARTYPE* screen;
 #else
 #define CHARTYPE char
-    CHARTYPE* screen;
 #endif
+    CHARTYPE* screen;
 
 public:
     ConsoleViewport() {
+#ifdef _WIN32
         hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         width = csbi.srWindow.Right - csbi.srWindow.Left;
         height = csbi.srWindow.Bottom - csbi.srWindow.Top;
+#elif __linux__
+        struct winsize w;
+        ioctl(fileno(stdout), TIOCGWINSZ, &w);
+        width = (int)(w.ws_col);
+        height = (int)(w.ws_row);
+#endif // Windows/Linux
+
         widthP1 = width + 1;
         screenSize = widthP1 * height;
         screen = new CHARTYPE[screenSize];
@@ -37,7 +55,7 @@ public:
         delete[] screen;
     }
 
-    void inline SafeScreen(int& x, int& y, CHARTYPE ch) noexcept {
+    void inline SafeScreen(int& x, int& y, CHARTYPE ch) {
         if (x >= width || y >= width)
             throw std::out_of_range("too big index");
         if (x < 0 || y < 0)
@@ -45,7 +63,7 @@ public:
         screen[x + y * widthP1] = ch;
     }
 
-    CHARTYPE inline SafeScreen(int& x, int& y) const noexcept {
+    CHARTYPE inline SafeScreen(int& x, int& y) const {
         if (x >= width || y >= width)
             throw std::out_of_range("too big index");
         if (x < 0 || y < 0)
@@ -62,11 +80,12 @@ public:
     }
 
     void inline Update() {
+#ifdef _WIN32
         SetConsoleCursorPosition(hStdOut, { 0, 0 });
-#if WIDECHAR
-        WriteConsoleW(hStdOut, screen, screenSize, NULL, NULL);
-#else
-        printf(screen);
+        WriteConsole(hStdOut, screen, screenSize, NULL, NULL);
+#elif __linux__
+        printf("%c[0;0f",0x1B);
+        printf("%ls", screen);
 #endif
     }
 
